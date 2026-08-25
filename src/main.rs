3310c6cebaddd4cbdf1ab9de72b987b39d3da233
Charlie Parsons <prisonofidentity@gmail.com>
2026-08-24 21:21:46 -0400

    Basic working server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, extract::Path, routing::get};
+
+async fn greet(Path(name): Path<String>) -> String {
+    format!("Hello, {name}!\n").to_string()
+}
+
+#[tokio::main]
+async fn main() {
+    // application currently has a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!\n" }))
+        .route("/{name}", get(greet));
+
+    // run app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
