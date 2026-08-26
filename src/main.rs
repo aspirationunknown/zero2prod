37e80bb4af1ca5dcf5a37f3fd0ad3e3c931d73e7
Charlie Parsons <prisonofidentity@gmail.com>
2026-08-26 17:22:51 -0400

    Added health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use axum::{Router, extract::Path, routing::get};
+use axum::{Router, extract::Path, http::StatusCode, routing::get};
 
 async fn greet(Path(name): Path<String>) -> String {
     format!("Hello, {name}!\n").to_string()
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
 #[tokio::main]
@@ -9,7 +13,8 @@ async fn main() {
     // application currently has a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!\n" }))
-        .route("/{name}", get(greet));
+        .route("/{name}", get(greet))
+        .route("/health_check", get(health_check));
 
     // run app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
