f8b42c688b90adca4130ba8366b659b86fad2868
Charlie Parsons <prisonofidentity@gmail.com>
2026-08-29 17:05:57 -0400

    Make init script more robust, added first migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,20 @@
 set -x
 set -eo pipefail
 
+# Check for dependencies
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed"
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed"
+    echo >&2 "Use:"
+    echo >&2 "    cargo install --version=0.9.0 sqlx-cli --no-default-features --features postgres"
+    echo >&2 "to install it"
+    exit 1
+fi
+
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
 
@@ -11,18 +25,22 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=zero2prod}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 
-# Check if a custom port has been set, otherwise default to '8079'
-DB_PORT="${POSTGRES_PORT:=8079}"
+# Check if a custom port has been set, otherwise default to '5432'
+DB_PORT="${POSTGRES_PORT:=5432}"
 
+# Allow for skipping docker, if the dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
 # Launch postgres using Docker
-docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":8079 \
-  -d postgres \
-  postgres -N 1000
-  # ^ Increased maximum number of connections for testing purposes
+    docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+    # ^ Increased maximum number of connections for testing purposes
+fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -31,7 +49,14 @@ until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q';
     sleep 1
 done
 
+# Indicate the database is ready to be created
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
 # Set up env var and create database
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 
 sqlx database create
+
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
