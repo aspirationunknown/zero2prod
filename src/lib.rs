0fd9a38a834f7a81628d2114a4fe5ef52f294891
Charlie Parsons <prisonofidentity@gmail.com>
2026-08-29 02:02:17 -0400

    Added subscription endpoint and associated tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,30 @@
-use axum::{Router, extract::Path, http::StatusCode, routing::get};
+use axum::{
+    Form, Router,
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
+};
+use serde::Deserialize;
 use tokio::net::TcpListener;
+
+#[derive(Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
 
 async fn greet(Path(name): Path<String>) -> String {
     format!("Hello, {name}!\n")
 }
 
 async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+async fn subscribe(subscription_form: Form<FormData>) -> StatusCode {
+    let name = subscription_form.0.name;
+    let email = subscription_form.0.email;
+    println!("Subscriber info: name = {name}, email = {email}");
     StatusCode::OK
 }
 
@@ -16,7 +35,8 @@ pub fn run(
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!\n" }))
         .route("/{name}", get(greet))
-        .route("/health_check", get(health_check));
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));
 
     listener.set_nonblocking(true)?;
     let listener = TcpListener::from_std(listener)?;
