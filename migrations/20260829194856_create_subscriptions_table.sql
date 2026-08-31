51ac45c76c4134174bb7bcdd92b36c941c5ba6cd
Charlie Parsons <prisonofidentity@gmail.com>
2026-08-30 20:09:09 -0400

    Fixed typo in table name

--- migrations/20260829194856_create_subscriptions_table.sql
+++ migrations/20260829194856_create_subscriptions_table.sql
@@ -1,6 +1,6 @@
 -- migrations/{timestamp}_create_subscriptions_table.sql
 -- Create Subscriptions Table
-CREATE TABLE subcriptions(
+CREATE TABLE subscriptions(
     id uuid NOT NULL,
     PRIMARY KEY (id),
     email TEXT NOT NULL UNIQUE,
