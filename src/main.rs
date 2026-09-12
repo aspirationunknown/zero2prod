0d7e47216168063c910cb91b1f3c79076b1f52aa
Charlie Parsons <prisonofidentity@gmail.com>
2026-09-12 14:49:14 -0400

    Refactor subscriber setup into telemetry module

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,17 @@
 
 use sqlx::postgres::PgPoolOptions;
 use std::time::Duration;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
     // This logger uses the RUST_LOG environment variable to determine log filtering level
     // or if it is not set then it defaults to "info" level logging
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        // Output the formatted spans to stdout
-        std::io::stdout,
-    );
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    // init_subscriber should only be ever called once
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPoolOptions::new()
         .max_connections(5)
